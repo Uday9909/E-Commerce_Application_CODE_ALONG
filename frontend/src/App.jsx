81ba28e264f4fe735ae8b1b57d2eb374c7ea2084
Udaybir Singh <uday@Udaybirs-MacBook-Air.local>
2025-01-13 14:03:16 +0530

    milestone 2 done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import {LoginPage} from './Routes/routes.js';
+
+const App=()=> {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
